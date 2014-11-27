f10bce484950c17de341df484554d8f225d769e8
jadefeng <j.feng.1991@gmail.com>
2014-11-27 17:24:44 +1100

    leaderboard for games up

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
-	has_many :games
+	has_many :plays
+
+	# Create a high score method that finds the best scores in each game
+	def highscore
+		plays = self.plays.order("score desc").limit(5)	#Orders by score in descending
+		return plays.to_a
+	end
 end
